5675cfd07ad23e40fa1908ed274f16eaf4c6223e
eviatar-ben <eviatar94@gmail.com>
2022-05-18 17:14:48 +0300

    initial commit

--- ex5.py
+++ ex5.py
@@ -3,10 +3,29 @@ import ex4
 import utilities
 
 
+# todo maybe in ex4 every track is missing the last frame
+# todo check wether the Rt thats got from ex3 are corresponding to the frame
+#  (maybe there is a 1 shift between the frames and the Rts??)
+# todo: should i multiply by K?
+def build_gtsam_frame(frame):
+    reversed_ext = utilities.reverse_ext(frame.extrinsic_mat)
+    R = reversed_ext[:, :3]
+    t = reversed_ext[:, 3]
+    gtsam_R = gtsam.Rot3(gtsam.Point3(R[:, 0]),
+                         gtsam.Point3(R[:, 1]),
+                         gtsam.Point3(R[:, 2]))
+    gtsam_t = gtsam.Point3(t)
+
+    k = utilities.K
+    fx, fy, skew, cx, cy = k[0, 0], k[1, 1], k[0, 1], k[0, 2], k[1, 2]
+    gtsam_K = gtsam.Cal3_S2Stereo(fx, fy, skew, cx, cy, -utilities.M2[0, 3])
+
+
 def get_gtsam_frames(frames):
     gtsam_frames = []
     for frame in frames:
-        gtsam_frames.append()
+        gtsam_frame = build_gtsam_frame(frame)
+        gtsam_frames.append(gtsam_frame)
 
 
 def f():
@@ -17,4 +36,5 @@ def f():
 
 
 if __name__ == '__main__':
+    # 5.1
     f()
