43a0f74994cc28d6ac53b3250243d59eaaab6a44
eviatar-ben <eviatar94@gmail.com>
2022-05-22 12:06:12 +0300

    pulling ex3 cupdates

--- ex4.py
+++ ex4.py
@@ -128,7 +128,7 @@ def main():
     # 4.2
     db.present_statistics()
     # 4.3
-    track = utilities.get_track_in_len(db, 11, True)
+    track = utilities.get_track_in_len(db, 3, False)
     exs_plots.display_track(db, track, crop=False)
     # 4.4
     exs_plots.connectivity_graph(db.frames.values())
